8bb1ddb4bb49a61c9096f65bc571583fc89d09d1
TakuyaIsaki <takuya.isaki@play.jp>
2023-09-21 02:04:56 +0900

    udpate sha256

--- jobcan-manipulator.rb
+++ jobcan-manipulator.rb
@@ -2,7 +2,7 @@ class JobcanManipulator < Formula
   desc "Jobcan auto manipulate on terminal"
   homepage "https://github.com/Taku3939/jobcan-manipulator"
   url "https://github.com/Taku3939/jobcan-manipulator/releases/download/0.0.1/jobcan-manipulator.zip"
-  sha256 "c8306ffbebd4787a4f8e41051c392c478e90d63a766b89b16455271c2f6980e3"
+  sha256 "9092696607fd8d20138716000d1de7ddc699606a9721de8b3c2a461c6738dd9e"
   license "MIT"
   version "0.0.1"
   def install
