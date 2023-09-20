c26e68576c1318148d4c7a2e0fb5cf6c1fc597b3
TakuyaIsaki <takuya.isaki@play.jp>
2023-09-21 00:48:39 +0900

    update

--- jobcan-manipulator.rb
+++ jobcan-manipulator.rb
@@ -1,12 +1,15 @@
 class JobcanManipulator < Formula
-    desc 'Jobcan auto manipulate on terminal'
-    homepage 'https://github.com/Taku3939/jobcan-manipulator'
-    url 'https://github.com/Taku3939/jobcan-manipulator/releases/download/0.0.1/jobcan-manipulator.tar.gz'
-    sha256 '1b9cce549110494d21f858f96277b934b94f8f1ba13c7ed86ca329321d486e78'
-    license ''
-  
-    def install
-      bin.install 'jobcan-manipulator'
-    end
+  desc "Jobcan auto manipulate on terminal"
+  homepage "https://github.com/Taku3939/jobcan-manipulator"
+  url "https://github.com/Taku3939/jobcan-manipulator/releases/download/0.0.1/jobcan-manipulator.zip"
+  sha256 "1b9cce549110494d21f858f96277b934b94f8f1ba13c7ed86ca329321d486e78"
+  license "MIT"
+  version "0.0.1"
+  def install
+    bin.install "jobcan-manipulator"
+  end
+
+  test do
+    system "true"
+  end
 end
-  
