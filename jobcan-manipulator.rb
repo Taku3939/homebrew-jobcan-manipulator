4c9b71adcb2f58b62411a1213fd0a3ffbba1924f
TakuyaIsaki <takuya.isaki@play.jp>
2025-04-30 14:29:23 +0900

    v3に変更

--- jobcan-manipulator.rb
+++ jobcan-manipulator.rb
@@ -1,10 +1,10 @@
 class JobcanManipulator < Formula
   desc "Jobcan auto manipulate on terminal"
   homepage "https://github.com/Taku3939/jobcan-manipulator"
-  url "https://github.com/Taku3939/jobcan-manipulator/releases/download/0.0.2/jobcan-manipulator.zip"
-  sha256 "ae8b23754b3a1580e82f38df5b44a7aa35a7a6362fed1320abdf24383a80c760"
+  url "https://github.com/Taku3939/jobcan-manipulator/releases/download/0.0.3/jobcan-manipulator.zip"
+  sha256 "79a8803da40ead4bec04be72e0d026658f41db87a09401e9d0a35133a329a684"
   license "MIT"
-  version "0.0.2"
+  version "0.0.3"
   def install
     bin.install "jobcan-manipulator"
   end
